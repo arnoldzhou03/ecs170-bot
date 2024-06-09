f68c6035126a578340b0954528a741f35cb81a83
arnoldzhou03 <arnoldzhou03@gmail.com>
2024-06-08 18:01:04 -0700

    Updated engine

--- homemade.py
+++ homemade.py
@@ -10,12 +10,152 @@ from lib.engine_wrapper import MinimalEngine
 from lib.types import MOVE, HOMEMADE_ARGS_TYPE
 import logging
 
+MAX, MIN = 100000, -100000
+max_depth = 4
+
+piece_square_table = {
+    chess.PAWN: [
+        [0,  0,  0,  0,  0,  0,  0,  0],
+        [50, 50, 50, 50, 50, 50, 50, 50],
+        [10, 10, 20, 30, 30, 20, 10, 10],
+        [5,  5, 10, 25, 25, 10,  5,  5],
+        [0,  0,  0, 20, 20,  0,  0,  0],
+        [5, -5,-10,  0,  0,-10, -5,  5],
+        [5, 10, 10,-20,-20, 10, 10,  5],
+        [0,  0,  0,  0,  0,  0,  0,  0]
+    ],
+    chess.KNIGHT: [
+        [-50,-40,-30,-30,-30,-30,-40,-50],
+        [-40,-20,  0,  0,  0,  0,-20,-40],
+        [-30,  0, 10, 15, 15, 10,  0,-30],
+        [-30,  5, 15, 20, 20, 15,  5,-30],
+        [-30,  0, 15, 20, 20, 15,  0,-30],
+        [-30,  5, 10, 15, 15, 10,  5,-30],
+        [-40,-20,  0,  5,  5,  0,-20,-40],
+        [-50,-40,-30,-30,-30,-30,-40,-50]
+    ],
+    chess.BISHOP: [
+        [-20,-10,-10,-10,-10,-10,-10,-20],
+        [-10,  0,  0,  0,  0,  0,  0,-10],
+        [-10,  0,  5, 10, 10,  5,  0,-10],
+        [-10,  5,  5, 10, 10,  5,  5,-10],
+        [-10,  0, 10, 10, 10, 10,  0,-10],
+        [-10, 10, 10, 10, 10, 10, 10,-10],
+        [-10,  5,  0,  0,  0,  0,  5,-10],
+        [-20,-10,-10,-10,-10,-10,-10,-20]
+    ],
+    chess.ROOK: [
+        [0,  0,  0,  0,  0,  0,  0,  0],
+        [5, 10, 10, 10, 10, 10, 10,  5],
+        [-5,  0,  0,  0,  0,  0,  0, -5],
+        [-5,  0,  0,  0,  0,  0,  0, -5],
+        [-5,  0,  0,  0,  0,  0,  0, -5],
+        [-5,  0,  0,  0,  0,  0,  0, -5],
+        [-5,  0,  0,  0,  0,  0,  0, -5],
+        [0,  0,  0,  5,  5,  0,  0,  0]
+    ],
+    chess.QUEEN: [
+        [-20,-10,-10, -5, -5,-10,-10,-20],
+        [-10,  0,  0,  0,  0,  0,  0,-10],
+        [-10,  0,  5,  5,  5,  5,  0,-10],
+        [-5,  0,  5,  5,  5,  5,  0, -5],
+        [0,  0,  5,  5,  5,  5,  0, -5],
+        [-10,  5,  5,  5,  5,  5,  0,-10],
+        [-10,  0,  5,  0,  0,  0,  0,-10],
+        [-20,-10,-10, -5, -5,-10,-10,-20]
+    ],
+    chess.KING: [
+        [-30,-40,-40,-50,-50,-40,-40,-30],
+        [-30,-40,-40,-50,-50,-40,-40,-30],
+        [-30,-40,-40,-50,-50,-40,-40,-30],
+        [-30,-40,-40,-50,-50,-40,-40,-30],
+        [-20,-30,-30,-40,-40,-30,-30,-20],
+        [-10,-20,-20,-20,-20,-20,-20,-10],
+        [20, 20,  0,  0,  0,  0, 20, 20],
+        [20, 30, 10,  0,  0, 10, 30, 20]
+    ]
+}
+
+def get_piece_value(piece):
+    values = {
+        chess.PAWN: 100,
+        chess.KNIGHT: 320,
+        chess.BISHOP: 330,
+        chess.ROOK: 500,
+        chess.QUEEN: 900,
+        chess.KING: 20000
+    }
+    return values[piece.piece_type] if piece.color == chess.WHITE else -values[piece.piece_type]
+
+
+def evaluate_board(board):
+    if board.is_checkmate():
+        return MIN if board.turn else MAX
+
+    material = 0
+    positional = 0
+    for square, piece in board.piece_map().items():
+        piece_value = get_piece_value(piece)
+        material += piece_value
+
+        row, col = divmod(square, 8)
+        positional_value = piece_square_table[piece.piece_type][row][col]
+        positional += positional_value if piece.color == chess.WHITE else -positional_value
+
+    eval_value = material + positional
+    return eval_value
+
+def minimax(depth, maximizingPlayer, alpha, beta, board):
+    if depth == 0 or board.is_game_over():
+        eval_value = evaluate_board(board)
+        return eval_value, None
+
+    best_move = None
+
+    if maximizingPlayer:
+        best = MIN
+        for move in board.legal_moves:
+            board.push(move)
+            val, _ = minimax(depth - 1, False, alpha, beta, board)
+            board.pop()
+            if val > best:
+                best = val
+                best_move = move
+            alpha = max(alpha, best)
+            if beta <= alpha:
+                break
+    else:
+        best = MAX
+        for move in board.legal_moves:
+            board.push(move)
+            val, _ = minimax(depth - 1, True, alpha, beta, board)
+            board.pop()
+            if val < best:
+                best = val
+                best_move = move
+            beta = min(beta, best)
+            if beta <= alpha:
+                break
+    return best, best_move
 
 # Use this logger variable to print messages to the console or log files.
 # logger.info("message") will always print "message" to the console or log file.
 # logger.debug("message") will only print "message" if verbose logging is enabled.
 logger = logging.getLogger(__name__)
 
+
+
+class ECS170Engine(MinimalEngine):
+
+    def search(self, board: chess.Board, *args: HOMEMADE_ARGS_TYPE) -> PlayResult:
+        _, best_move = minimax(max_depth, board.turn, MIN, MAX, board)
+        if best_move:
+            return PlayResult(best_move, None)
+        else:
+            print("(random move)")
+            return PlayResult(random.choice(list(board.legal_moves)), None)
+
+        # return PlayResult(random.choice(list(board.legal_moves)), None)
 
 class ExampleEngine(MinimalEngine):
     """An example engine that all homemade engines inherit."""
